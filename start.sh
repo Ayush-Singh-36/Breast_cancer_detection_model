d82622cfb5bbb09ae9b3d026c25491015764f0a1
Ayush-Singh-36 <ayushsingh3962@gmail.com>
2026-08-01 14:41:47 +0530

    del: removed dockerfile, app.py, and start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Spin up FastAPI on port 8000
-python -m uvicorn app:app --host 0.0.0.0 --port 8000 &
-
-# Spin up Streamlit UI on port 8501
-python -m streamlit run frontend.py --server.port 8501 --server.address 0.0.0.0
